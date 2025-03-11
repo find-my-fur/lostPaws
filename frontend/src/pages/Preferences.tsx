e53ac93abed763322aa6f9c1d927e98943ade5f2
Jayson Sanon <jaysonsanon@Jaysons-MacBook-Air.local>
2025-03-11 15:11:18 -0400

    install nodemon as a dependency

--- frontend/src/pages/Preferences.tsx
+++ frontend/src/pages/Preferences.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
 
 const Preferences = () => {
-  return <div>Preferences Page</div>;
+  const input: string[] = ['Name', 'Address', 'State', 'City', 'Zip', 'Phone'];
+  const inputElement: React.ReactElement[] = [];
+
+  for (const elem of input) {
+    inputElement.push(
+      <label key={elem}>
+        Enter {elem} <input name={elem} />
+      </label>
+    );
+  }
+
+  const submitSurvey = (formData): void => {
+    // for (const elem of input) {
+      
+    // }
+
+    fetch('');
+
+    const query: string = formData.get('query');
+    console.log(query)
+    return;
+  };
+
+  return (
+    <div id='surveycontainer'>
+      <form action={submitSurvey}>
+        {inputElement}
+        <button type='submit'>Search</button>
+      </form>
+    </div>
+  );
 };
 
 export default Preferences;
