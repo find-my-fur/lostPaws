49ca9d8f1a0a70e742b2fa0bd61d6826eb732b3a
SierraR-AI <elevatewithsierra@gmail.com>
2025-03-11 12:11:39 -0400

    Front End File Structure

---
+++ frontend/src/components/PetCard.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PetCard = () => {
+  return <div>Pet Card</div>;
+};
+
+export default PetCard;
---
+++ frontend/src/pages/Preferences.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Preferences = () => {
+  return <div>Preferences Page</div>;
+};
+
+export default Preferences;
