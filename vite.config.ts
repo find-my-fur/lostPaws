28cc09754650c3f10a28315f1277d16ab2e9d0a8
Jose Andrew <Jandrew205@outlook.com>
2025-03-17 21:28:59 -0400

    deployed frontend

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-
   server: {
     // port: 8080,
     open: true,
